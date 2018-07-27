ccd545e26838b3db1db5595e7ffa7512745e67f2
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-27 21:28:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,7 @@ class Application
       end
     elsif req.path.match(/add/)
       @@cart << @@items[0]
-        if @@cart.include?("#{@@items[0]}")
+        if @@cart.includes?("#{@@items[0]}")
           resp.write "added #{@@items[0]}"
         else
           resp.write "We don't have that item"
